67bfd8946d01bfe0b060384ea89e919eaadde920
ChiragMR27 <chiragmr676@gmail.com>
2025-04-10 20:12:41 +0530

    Update MongoDB IP access and clean config

--- src/main/java/com/jobportal/onlinejobportal/OnlineJobPortalApplication.java
+++ src/main/java/com/jobportal/onlinejobportal/OnlineJobPortalApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineJobPortalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineJobPortalApplication.class, args);
-    }
+	public static void main(String[] args) {
+	    System.setProperty("jdk.tls.client.protocols", "TLSv1.2"); // force TLSv1.2 for MongoDB Atlas
+	    SpringApplication.run(OnlineJobPortalApplication.class, args);
+	}
+
 }
